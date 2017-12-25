86b8fe57c89892a33ce08ff76486b9e0d5da643e
Ertugrul Söylemez <esz@posteo.de>
2017-12-25 06:40:49 +0100

    Bump: v1.0.0.1

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, ansi-terminal, async, base, stdenv, stm }:
 mkDerivation {
   pname = "progress-meter";
-  version = "1.0.0";
+  version = "1.0.0.1";
   src = ./.;
   libraryHaskellDepends = [ ansi-terminal async base stm ];
   homepage = "https://github.com/esoeylemez/progress-meter";
